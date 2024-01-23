fd729ef00e01e1dbfc3ba258f87f5fd39766fe5e
makeda banza <makedabanza16@gmail.com>
2024-01-23 12:49:44 +0200

    hover on react icon

---
+++ src/components/ReactIcon.jsx
@@ -0,0 +1,13 @@
+function ReactIcon(props) {
+  return (
+    <>
+      <div className="flex flex-row">
+          {/* <svg onClick={props.onClickReact} width="20" height="20" viewBox="0 0 24 24" className={props.fill} xmlns="http://www.w3.org/2000/svg"> {props.children} </svg> */}
+          <svg onClick={props.onClickReact} width="20" height="20" viewBox={props.viewBoxIcon} className={props.fill} xmlns="http://www.w3.org/2000/svg"> {props.children} </svg>
+        <small className={props.classReact} >{props.react}</small>
+      </div>
+    </>
+  );
+}
+export default ReactIcon
+// "fill-cyan-700 hover:fill-red-700" "text-white text-xs opacity-50 mt-1 ml-1 "
